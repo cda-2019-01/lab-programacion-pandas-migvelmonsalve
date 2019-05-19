115d2fbdfced3e53a74a92bf33a201d4a17ecf40
Miguel Angel Monsalve Arango <50241610+migvelmonsalve@users.noreply.github.com>
2019-05-18 20:34:19 -0500

    Add files via upload

--- q02.py
+++ q02.py
@@ -1,5 +1,21 @@
 ##
-## Imprima el promedio de la _c2 por cada letra de la _c1 
-## de la tabla tbl0
-## 
-
+## Imprima la cantidad de registros por letra para la 
+## primera columna, ordenados alfabeticamente.
+##
+## A,8
+## B,7
+## C,5
+## D,6
+## E,14
+##
+text = []
+with open('data.csv', 'rt') as f:
+        text += f.readlines()
+        
+text = [line.replace('\t',',') for line in text]
+text = [line.split(',') for line in text]
+text2 = [line[0] for line in text]
+text3 = list(set(text2))
+text3.sort()
+for letra in text3:
+    print('{},{}'.format(letra,text2.count(letra)))
