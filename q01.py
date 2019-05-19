5757bda126b1908ca8657f88511ba313113814ae
Miguel Angel Monsalve Arango <50241610+migvelmonsalve@users.noreply.github.com>
2019-05-18 20:14:26 -0500

    Add files via upload

--- q01.py
+++ q01.py
@@ -1,4 +1,16 @@
 ##
-## Imprima la cantidad de registros por cada letra 
-## de la columna _c1 de la tabla tbl0
-## 
+## Imprima la suma de la segunda columna.
+##
+text = []
+with open('data.csv', 'rt') as f:
+    text += f.readlines()
+
+text = [line.replace('\t',',') for line in text]
+text = [line.split(',') for line in text]
+
+x = 0
+
+for line in text:
+    x = x + int(line[1])
+
+print(x)
