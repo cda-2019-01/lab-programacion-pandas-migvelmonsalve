2f7f3389c725a1ca32a352898139b1c09906e3ec
Miguel Angel Monsalve Arango <50241610+migvelmonsalve@users.noreply.github.com>
2019-06-03 09:33:02 -0500

    Add files via upload

--- q12.py
+++ q12.py
@@ -3,5 +3,23 @@
 ## y tbl2.tsv, compute la suma de tbl2._c5b por cada
 ## valor en tbl0._c1.
 ## 
+import pandas as pd
+import numpy as np
+pd.set_option('display.notebook_repr_html', False)
 
+df = pd.read_csv('tbl2.tsv',  # el archivo
+                    sep = '\t',         # separador de campos
+                    thousands = None,  # separador de miles para números
+                    decimal = '.') 
+
+df2 = pd.read_csv('tbl0.tsv',  # el archivo
+                    sep = '\t',         # separador de campos
+                    thousands = None,  # separador de miles para números
+                    decimal = '.') 
+
+df = df.join(df2,how='left',on='_c0',lsuffix='_caller', rsuffix='_other')
+df = df[['_c5a','_c2']]
+df = df.sort_values('_c5a')
+x = df.groupby('_c5a')['_c2'].sum()
+print(x)
 
